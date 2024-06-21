17711c2bf16089adf37b6e6fa06e937888519a4d
matejsemancik <matej.semancik@gmail.com>
2024-06-21 13:48:42 +0200

    Create project from Android Studio template

---
+++ app/src/main/java/app/futured/meridian_multiprocess_repro/MainActivity.kt
@@ -0,0 +1,47 @@
+package app.futured.meridian_multiprocess_repro
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import app.futured.meridian_multiprocess_repro.ui.theme.MeridianMultiprocessBugTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MeridianMultiprocessBugTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MeridianMultiprocessBugTheme {
+        Greeting("Android")
+    }
+}
